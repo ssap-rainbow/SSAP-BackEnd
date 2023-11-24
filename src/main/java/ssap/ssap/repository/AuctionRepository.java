48a91738e44d13a69c4a36cf1dfaac3fe772b900
cipp00h <12seokeun@gmail.com>
2023-11-24 22:22:43 +0900

    feature: 심부름 상세 정보 조회 로직 수정
    - taskId를 통해 Auction 엔티티를 찾는 로직 수정

--- src/main/java/ssap/ssap/repository/AuctionRepository.java
+++ src/main/java/ssap/ssap/repository/AuctionRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ssap.ssap.domain.Auction;
 
+import java.util.Optional;
+
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
+    Optional<Auction> findByTaskId(Long taskId);
 }
