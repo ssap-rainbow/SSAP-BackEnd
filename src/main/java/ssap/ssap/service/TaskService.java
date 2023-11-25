a60b797da13f7c13c4006c425f19766aec59e9d0
cipp00h <12seokeun@gmail.com>
2023-11-26 04:00:28 +0900

    fix: tasks 테이블에서 aution_id 외래키 참조 값이 null로 저장되는 문제
    tasks 테이블에 autionId 설정 하는 로직 추가

--- src/main/java/ssap/ssap/service/TaskService.java
+++ src/main/java/ssap/ssap/service/TaskService.java
@@ -104,8 +104,6 @@ public class TaskService {
         task.setCategory(category);
         task.setDetailedItem(detailedItem);
 
-        taskRepository.save(task); // task 저장
-
         if (task.getAuctionStatus()) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
             LocalDateTime auctionStartTime = LocalDateTime.parse(createForm.getAuctionStartTime(), formatter);
@@ -115,8 +113,12 @@ public class TaskService {
             auction.setTask(task);
             auction.setStartTime(auctionStartTime);
             auction.setEndTime(auctionEndTime);
+            task.setAuction(auction); // 여기에서 Task에 Auction ID 설정
+
             auctionRepository.save(auction);
         }
+
+        taskRepository.save(task);
 
         return task;
     }
